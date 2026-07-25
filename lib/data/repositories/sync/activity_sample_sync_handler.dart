543ddddad9ffc0ddd41e77ab3ba81d0b9d079751
Ashutosh Das <git.pyprism@gmail.com>
2026-07-25 21:47:23 +0600

    refactored date formatting in activity sample sync handler

--- lib/data/repositories/sync/activity_sample_sync_handler.dart
+++ lib/data/repositories/sync/activity_sample_sync_handler.dart
@@ -1,6 +1,6 @@
 import 'package:drift/drift.dart';
-import 'package:intl/intl.dart';
 
+import '../../../shared/format/app_date_format.dart';
 import '../../local/database.dart';
 import '../../local/sync_wire.dart';
 import '../../local/tables/activity_samples_table.dart';
@@ -161,7 +161,7 @@ class ActivitySampleSyncHandler implements SyncTypeHandler {
             typeName: typeName,
             id: r.id,
             title: r.activityType.name,
-            subtitle: DateFormat.yMMMd().add_jm().format(r.startedAt.toLocal()),
+            subtitle: AppDateFormat.dateTime(r.startedAt.toLocal()),
           ),
         )
         .toList();
