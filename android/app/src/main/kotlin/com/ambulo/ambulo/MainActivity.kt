64b6974c9ffefecce231cfb0323fc93d139a8082
Ashutosh Das <git.pyprism@gmail.com>
2026-07-25 21:48:30 +0600

    updated MainActivity to extend FlutterFragmentActivity for Health Connect permissions support

--- android/app/src/main/kotlin/com/ambulo/ambulo/MainActivity.kt
+++ android/app/src/main/kotlin/com/ambulo/ambulo/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ambulo.ambulo
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+// FlutterFragmentActivity (not FlutterActivity) is required by the `health`
+// package's Android 14+ Health Connect permissions-rationale flow.
+class MainActivity : FlutterFragmentActivity()
